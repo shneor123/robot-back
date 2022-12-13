6502cbbd1a754c42197146c0e8e6e46831492c20
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-12-13 13:07:16 +0200

    ...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const http = require('http').createServer(app)
