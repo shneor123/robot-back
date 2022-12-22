6f060423cc870e13af5c45dd5bbb87a371223243
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-12-22 11:25:43 +0200

    fix conect back to front

--- services/socket.service.js
+++ services/socket.service.js
@@ -4,10 +4,6 @@ const robotService = require('../api/robot/robot.service.mongodb')
 
 let gIo = null
 
-module.exports = {
-    // set up the sockets service and define the API
-    setupSocketAPI,
-}
 
 function setupSocketAPI(http) {
     gIo = require('socket.io')(http, {
@@ -16,10 +12,31 @@ function setupSocketAPI(http) {
         }
     })
     gIo.on('connection', socket => {
-        // logger.info(`New connected socket [id: ${socket.id}]`)
+        console.log(`New connected socket [id: ${socket.id}]`)
         socket.on('disconnect', socket => {
-            // logger.info(`Socket disconnected [id: ${socket.id}]`)
+            console.log(`Socket disconnected [id: ${socket.id}]`)
         })
+
+        socket.on('join-board', boardId => {
+            console.log('joined board with id: ', boardId)
+            if (socket.boardId === boardId) return;
+            if (socket.boardId) {
+                socket.leave(socket.boardId);
+            }
+            socket.join(boardId);
+            socket.boardId = boardId;
+        });
+        socket.on('board-change', (updatedBoard) => {
+            // excludedSocket.broadcast.to(room).emit(type, data)
+            // async function broadcast({ type, data, room = null, userId }) {
+            console.log('hey new board');
+            gIo.to(socket.boardId).emit('update-board', updatedBoard)
+            // broadcast( )
+            // console.log('Emitting board change');
+            // broadcast('board-change').to(socket.boardId).emit('updated-board', updatedBoard);
+        });
+
+
         socket.on('chat-set-room', roomId => {
             let numOfConnectedUsers
             if (!roomId && socket.roomId) {
@@ -40,16 +57,31 @@ function setupSocketAPI(http) {
 
             //sending to a specific room
             gIo.to(socket.roomId).emit('chat-add-msg', msg)
-            
+
             //In this application the roomId is the robotId. Also, the chat appears no where else in the application
             robotService.addToChat(socket.roomId, msg)
         })
         socket.on('chat-fire-typing', fullname => {
             socket.broadcast.to(socket.roomId).emit('chat-subscribe-typing', fullname)
         })
+
+        socket.on('set-user-socket', userId => {
+            console.log(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
+            socket.userId = userId
+        })
+        socket.on('unset-user-socket', () => {
+            console.log(`Removing socket.userId for socket [id: ${socket.id}]`)
+            delete socket.userId
+        })
+
+
     })
 }
 
 function _getNumOfConnectedUsers(roomId, diff) {
     return gIo.sockets.adapter.rooms.get(roomId).size + diff
+}
+module.exports = {
+    // set up the sockets service and define the API
+    setupSocketAPI,
 }
